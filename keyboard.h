9d7d915550ae3ebf8c00390032678301faa71337
saeeedhany <alsaeedalbasi0ny@gmail.com>
2026-02-10 10:01:57 +0200

    remove everything (bacause it's unstable)

--- keyboard.h
+++
@@ -1,9 +0,0 @@
-#ifndef KEYBOARD_H
-#define KEYBOARD_H
-
-#include "kernel.h"
-
-void keyboard_init(void);
-char keyboard_getchar(void);
-
-#endif
--- syscall.h
+++
@@ -1,9 +0,0 @@
-#ifndef SYSCALL_H
-#define SYSCALL_H
-
-#include "kernel.h"
-
-void syscall_init(void);
-void syscall_test(void);
-
-#endif
